36230de56cf7e0a3e075e060a3225a269accc8fd
Haris Shahid <harisshahid00@gmail.com>
2017-09-21 19:47:24 +0500

    updated project after expo

---
+++ patienttracker2/index.android.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View , AppRegistry } from 'react-native';
+import SimpleApp from './src/main'
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <SimpleApp />
+      </View>
+    );
+  }
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop:20 ,
+  },
+});
+
+AppRegistry.registerComponent('patienttracker2', () => App);
